bc77147d4f3707304f3721c81a821fcdc8271ecf
Spidy-6 <bhupenkumardas2008@gmail.com>
2025-09-29 17:40:18 +0530

    Module 2 Assignments

---
+++ Assignment-1.1.py
@@ -0,0 +1,9 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num2 = int(num2)
+num1 = int(num1)
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("Addition =", add, "\nSubtraction =", sub, "\nMultiplication =", mul, "\nDivision =", div, "\n-----Thank You!-----")
