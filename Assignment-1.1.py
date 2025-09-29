6df1ce965b8d56d65acfab5f4f6d2aab0e88ce92
Spidy-6 <bhupenkumardas2008@gmail.com>
2025-09-29 17:45:47 +0530

    Assignment 1 & 2

--- Assignment-1.1.py
+++ Assignment-1.1.py
@@ -6,4 +6,5 @@ add = num1 + num2
 sub = num1 - num2
 mul = num1 * num2
 div = num1 / num2
+
 print("Addition =", add, "\nSubtraction =", sub, "\nMultiplication =", mul, "\nDivision =", div, "\n-----Thank You!-----")
