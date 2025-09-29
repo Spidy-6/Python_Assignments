763513b222f2a0017d727ef3bfb811e6d9489d3f
Spidy-6 <bhupenkumardas2008@gmail.com>
2025-09-29 16:15:55 +0530

    Second Push

---
+++ Greeting.py
@@ -0,0 +1,4 @@
+firstName = input("Enter your First Name: ")
+lastName = input("Enter your Second Name: ")
+fullName = firstName + " " + lastName + "!"
+print("Hey", fullName, "I hope you're loving my code;)")
---
+++ Test.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+a = input("Enter your name: ")
+print("Your Full Name is: ", a)
