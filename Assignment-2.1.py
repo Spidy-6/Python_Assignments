811f201f9bac383452f59aafb59cd565ba516049
Spidy-6 <bhupenkumardas2008@gmail.com>
2025-10-04 21:38:17 +0530

    Assignments 2.1 & 2.2

---
+++ Assignment-2.1.py
@@ -0,0 +1,12 @@
+num = int(input("Enter an integer number: "))
+if num % 2 != 0:
+    print(f"{num} is an odd number")
+else:
+    print(f"{num} is an even number")
+print("Thank You!")
+
+# which line is better? -->
+# num = int(input("Enter an integer number:"))
+# OR,
+# num = input("Enter an integer number: ")
+# Please state in the feedback
---
+++ Assignment-2.2.py
@@ -0,0 +1,5 @@
+Sum = 0
+for i in range(1, 51):
+    Sum += i
+print(f"\nThe sum of all the integers from 1 to 50 is: {Sum}")
+print("\n\t Thank You! \t")
