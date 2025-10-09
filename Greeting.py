62c68fb44b919c665f752f8e87a5623985b50e57
Bhupen Kumar Das <bhupenkumardas2008@gmail.com>
2025-10-09 20:49:42 +0530

    Delete Greeting.py

--- Greeting.py
+++
@@ -1,4 +0,0 @@
-firstName = input("Enter your First Name: ")
-lastName = input("Enter your Second Name: ")
-fullName = firstName + " " + lastName + "!"
-print("Hey", fullName, "I hope you're loving my code;)")
