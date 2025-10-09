3d2a82e9d15b7345f6551a86602c8275acdd274a
Spidy-6 <bhupenkumardas2008@gmail.com>
2025-10-09 22:38:14 +0530

    Assignment_4

--- Task_1.py
+++ Task_1.py
@@ -1,10 +1,9 @@
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-num = int(input("Enter a number: "))
-result = factorial(num)
-print(f"The factorial of {num} = {result}")
-print("Thank You!")
+try:
+    file1 = open('Sample.txt', 'r')
+except FileNotFoundError:
+    print("Error: The file was not found.")
+else:
+    fileline1 = file1.readline()
+    fileline2 = file1.readline()
+    print(f"Reading file content...\nLine 1: {fileline1}Line 2: {fileline2}")
+    file1.close()
--- Task_2.py
+++ Task_2.py
@@ -1,6 +1,11 @@
-from math import *
-
-num = int(input("Enter a number: "))
-print(f"Square root of {num} is : {sqrt(num)}")
-print(f"Natural logarithm of {num} is : {log(num)}")
-print(f"Square root of {num} is : {sin(num)}")
+text1 = input("Enter text to write to the file: ")
+with open("Output.txt", "w") as file1:
+    file1.write(text1)
+    print("Data successfully written to 'Otput.txt'\n")
+text2 = "\n" + input("Enter additional text to append: ")
+with open("Output.txt", "a") as file2:
+    file2.write(text2)
+    print("Data successfully appended in 'Output.txt'\n")
+with open("Output.txt", "r") as file3:
+    file_read = file3.read()
+    print(f"Final content of 'Output.txt':\n{file_read}")
